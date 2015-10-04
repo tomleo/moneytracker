00566c874ca30ffee0408e189554f0934e9f0895
Tom Leo <tom@tomleo.com>
2015-10-03 22:24:41 -0400

    Added UserSerializer

--- mt/mt/urls.py
+++ mt/mt/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, viewsets
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
+from spending.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -35,6 +30,7 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'', include('spending.urls')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+    url(r'^$', include('spending.urls')),
 ]
--- mt/spending/serializers.py
+++ mt/spending/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from . import models
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
