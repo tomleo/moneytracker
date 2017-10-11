d3c793b2ade5a0589de38a76be3661789ed6e899
Tommy <tomjleo@gmail.com>
2017-10-11 12:12:44 -0400

    Fix urls

--- mt/spending/urls.py
+++ mt/spending/urls.py
@@ -5,12 +5,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='spending/index.html')),
-    url(r'^add_category/$', views.AddCategory.as_view(), name="add-category"),
-    url(r'^add_note/$', views.AddNote.as_view(), name="add-note"),
-    url(r'^add_place/$', views.AddPlace.as_view(), name="add-place"),
-    url(r'^add_spending/$', views.AddSpending.as_view(), name="add-spending"),
-    url(r'^list_category/$', views.ListCategory.as_view(), name="list-category"),
-    url(r'^list_note/$', views.ListNote.as_view(), name="list-note"),
-    url(r'^list_place/$', views.ListPlace.as_view(), name="list-place"),
-    url(r'^list_spending/$', views.ListSpending.as_view(), name="list-spending"),
+    url(r'^add_category/$', views.CategoryListView.as_view(), name="add-category"),
+    url(r'^add_note/$', views.NoteListView.as_view(), name="add-note"),
+    url(r'^add_place/$', views.PlaceListView.as_view(), name="add-place"),
+    url(r'^add_spending/$', views.SpendingListView.as_view(), name="add-spending"),
+
+    url(r'^list_category/$', views.CategoryListView.as_view(), name="list-category"),
+    url(r'^list_note/$', views.NoteListView.as_view(), name="list-note"),
+    url(r'^list_place/$', views.PlaceListView.as_view(), name="list-place"),
+    url(r'^list_spending/$', views.SpendingListView.as_view(), name="list-spending"),
 ]
